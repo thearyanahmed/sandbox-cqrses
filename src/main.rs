0b5dd49b121c821a0b55461601ac249b5a48e8ff
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-10-01 20:49:42 +0200

    WIP (Unfinished)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,47 @@
+use std::path::Path;
+use std::sync::Arc;
+use axum::extract::{Extension, Path};
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use axum::routing::get;
+use axum::{Json, Router};
+use crate::domain::aggregate::BankAccount;
+
 mod domain;
 mod services;
 
 #[tokio::main]
-fn main() {
-    
+async fn main() {
+    let pool = default_postgress_pool("postgresql://demo_user:demo_pass@localhost:5432/demo").await;
+    let (cqrs, account_query) = cqrs_framework(pool);
+
+    // Configure the Axum routes and services.
+    // For this example a single logical endpoint is used and the HTTP method
+    // distinguishes whether the call is a command or a query.
+    let router = Router::new()
+        .route(
+            "/account/:account_id",
+            get(query_handler).post(command_handler),
+        )
+        .layer(Extension(cqrs))
+        .layer(Extension(account_query));
+
+    // Start the Axum server.
+    axum::Server::bind(&"0.0.0.0:3030".parse().unwrap())
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn query_handler(
+    Path(account_id): Box<Path>,
+    Extension(view_repo): Extension<Arc<PostgresViewRepository<BankAccountView, BankAccount>>>,
+) -> Response {
+    let view = match view_repo {
+
+    };
+}
+
+async fn command_handler() -> Response {
+
 }
--- src/services.rs
+++ src/services.rs
@@ -1,14 +1,36 @@
 pub struct AtmError;
 pub struct CheckingError;
 
-pub struct BankAccountServices;
+pub struct BankAccountServices {
+    pub services: Box<dyn BankAccountApi>
+}
 
 impl BankAccountServices {
-    async fn atm_withdrawal(&self, atm_id: &str, amount: f64) -> Result<(), AtmError> {
-        Ok(())        
+    pub fn new(services: Box<dyn BankAccountApi>) -> Self {
+        Self { services }
+    }
+}
+
+pub trait BankAccountApi: Sync + Send {
+    async fn atm_withdrawal(&self, atm_id: &str, amount: f64) -> Result<(), AtmError>;
+    async fn validate_check(&self, account_id: &str, check: &str) -> Result<(), CheckingError>;
+}
+
+// A very simple "happy path" set of services that always succeed.
+pub struct HappyPathBankAccountServices;
+
+#[async_trait]
+impl BankAccountApi for HappyPathBankAccountServices {
+    async fn atm_withdrawal(&self, _atm_id: &str, _amount: f64) -> Result<(), AtmError> {
+        println!("this is atm withdrawal");
+        Ok(())
     }
 
-    async fn validate_check(&self, account: &str, check: &str) -> Result<(), CheckingError> {
+    async fn validate_check(
+        &self,
+        _account_id: &str,
+        _check_number: &str,
+    ) -> Result<(), CheckingError> {
         Ok(())
     }
 }
